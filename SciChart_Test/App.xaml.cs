33be98abedaec5b6ef3fb5046eb8cdab38123cff
昀澍 蔡 <vcloudtree@hotmail.com>
2025-03-26 10:21:10 +0800

    加入專案檔案。

---
+++ SciChart_Test/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SciChart.Charting.Visuals;
+
+namespace SciChart_Test
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        // Set this code once in App.xaml.cs or application startup
+        SciChartSurface.SetRuntimeLicenseKey("s/dqyY2y2VksXrLi9ubscnGZV5QjZ30ZE12DrSDuVsPqINV36NuY8aBfJxMWN2Srb247vfJ1KVEQtuN+TpuNmY/J8EprZ6R1nnKcZtK9M9nBBnY6KeJ5XkUAMCAjOXTi5RnJopjLHjvTv0KNvq3dYMWd2TS/3gZKWG/TCmQImUscz8FcTznX4tafTJcezIq+YTrd935JOwLTjfiU/EFJ1MHuVFRsh/eC5aO8Zme7Kx6fMzMmho9B1+YSUsmsB7oUkawKAEgaGzJvH4snAQ3I4YrsIBkosgEANBlSe78sTZDhrpTcEXlhXNkt/U/35bi9Wxilz4wtUp6u2FaF3KqzsQrFT4K1hmJFk3087DBcpfDVIC+g1sJprkbKy1NGT7cQymUVZeyXHyYpPG9GZaU4VVHdn0BrcGx6Pf7RoZIw51UX/eqLwM8affmb7o+R6wK/Y66VUrKx8DrzniCrGDE=");
+    }
+}
---
+++ SciChart_Test/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using SciChart.Charting.ChartModifiers;
+using SciChart.Charting.Visuals;
+using SciChart.Charting.Visuals.Annotations;
+using SciChart.Charting.Visuals.Axes;
+using System.Windows;
+
+namespace SciChart_Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create the chart surface
+            var sciChartSurface = new SciChartSurface();
+
+            // Create the X and Y Axis
+            var xAxis = new NumericAxis() { AxisTitle = "Number of Samples (per series)" };
+            var yAxis = new NumericAxis() { AxisTitle = "Value" };
+
+            sciChartSurface.XAxis = xAxis;
+            sciChartSurface.YAxis = yAxis;
+
+            // Specify Interactivity Modifiers
+            sciChartSurface.ChartModifier = new ModifierGroup(new RubberBandXyZoomModifier(), new ZoomExtentsModifier());
+            // Add annotation hints to the user
+            var textAnnotation = new TextAnnotation()
+            {
+                Text = "Hello World!",
+                X1 = 5.0,
+                Y1 = 5.0
+            };
+            sciChartSurface.Annotations.Add(textAnnotation);
+        }
+    }
+}
